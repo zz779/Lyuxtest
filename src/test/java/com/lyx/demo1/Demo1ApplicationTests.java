ce92f10e416dfb19795594911837bbcada5d0e5a
lyx <111>
2024-07-30 11:37:53 +0800

    jkbxhjkxsfkl

---
+++ src/test/java/com/lyx/demo1/Demo1ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lyx.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("1231312312312");
+    }
+
+}
